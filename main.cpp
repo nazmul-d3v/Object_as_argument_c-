56d4ad68357ecfad701fc0b7eb58aa2541ea9a3f
Nazmul Islam <124438941+nazmul-d3v@users.noreply.github.com>
2024-12-26 11:04:40 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
+
+class demo
+{
+
+    int x;
+    static int y;
+public:
+    void getdata(int a)
+    {
+        x=a;
+        y=y+1;
+    }
+    void putdata()
+    {
+        cout<<"\n x="<<x<<"\n y="<<y;
+
+    }
+
+    static void abc()
+    {
+        cout<<"\n y="<<y;
+    }
+};
+int demo::y;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    demo aa;
+
+    aa.getdata(10);
+    aa.putdata();
+    demo::abc();
     return 0;
+
 }
